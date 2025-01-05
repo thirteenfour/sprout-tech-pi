c8f95eec5527d7f1fbae4df467ea6da75d34969b
thirteenfour <nikko.gammad@gmail.com>
2025-01-05 15:31:43 +0800

    try camera

--- sprouttech_pi.py
+++ sprouttech_pi.py
@@ -1,11 +1,24 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+import cv2
 
 app = QApplication([])
 win = QMainWindow()
 win.setWindowTitle("Sprout-Tech")
 win.resize(500,300)
 win.move(100,100)
+
+cam = cv2.VideoCapture(0)
+
+while True:
+	ret, image = cam.read()
+	cv2.imshow('Imagetest',image)
+	k = cv2.waitKey(1)
+	if k != -1:
+		break
+cv2.imwrite('/home/thirteenfour/Desktop/testimage.jpg', image)
+cam.release()
+cv2.destroyAllWindows()
 
 win.show()
 
